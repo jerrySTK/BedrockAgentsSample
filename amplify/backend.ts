957c5f7b6593e8a87f526f728855c39b56cca8ca
Gerardo Romero <gromero@sitek.mx>
2025-03-12 14:08:24 -0600

    lambda function

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,9 +1,11 @@
 import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
+import { toolsAgentHandler } from './functions/resource';
 
 /**
  * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
  */
 defineBackend({
   auth,
+  toolsAgentHandler
 });
